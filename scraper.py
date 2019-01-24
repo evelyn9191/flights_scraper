db7db1514136d30dd0e47d93ff95ae4be744c33a
Michaela Ockova <ockova.michaela@gmail.com>
2019-01-24 11:32:31 +0100

    Version 0.2 (unfinished)
    Scrapes all needed URLs. Database creation started.

--- scraper.py
+++ scraper.py
@@ -1,39 +1,66 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+import sqlite3
+import logging
+from datetime import datetime, timedelta
+
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
 
 class FlightsSpider(CrawlSpider):
     name = 'flights_spider'
-    start_urls = [
-        # PRG -> FAO
-        'http://www.azair.cz/azfin.php?tp=0&searchtype=flexi&srcAirport=Praha+%5BPRG%5D'
-        '&srcTypedText=prg&srcFreeTypedText=&srcMC=&srcFreeAirport='
-        '&dstAirport=Faro+%5BFAO%5D&dstTypedText=faro&dstFreeTypedText='
-        '&dstMC=&adults=1&children=0&infants=0&minHourStay=0%3A45&maxHourStay=23%3A20'
-        '&minHourOutbound=0%3A00&maxHourOutbound=24%3A00'
-        '&minHourInbound=0%3A00&maxHourInbound=24%3A00'
-        '&dstFreeAirport=&depdate=25.1.2019&arrdate=31.5.2019'
-        '&minDaysStay=3&maxDaysStay=9'
-        '&nextday=0&autoprice=true&currency=CZK'
-        '&wizzxclub=false&supervolotea=false&schengen=false&transfer=false'
-        '&samedep=true&samearr=false&dep0=true'
-        '&dep1=true&dep2=true&dep3=true&dep4=true&dep5=true&dep6=true'
-        '&arr0=true&arr1=true&arr2=true&arr3=true&arr4=true&arr5=true&arr6=true'
-        '&maxChng=1&isOneway=oneway&resultSubmit=Hledat#'
-        # PRG -> KEF &dstAirport=Reykjavik+%28Keflavik%29+%5BKEF%5D&dstTypedText=kef&dstFreeTypedText=&dstMC=&
-        # PRG -> TOS &dstAirport=Tromso+%5BTOS%5D&dstTypedText=tromso&dstFreeTypedText=&dstMC=&
-        # PRG -> DUB &dstAirport=Dublin+%5BDUB%5D&dstTypedText=dublin&dstFreeTypedText=&dstMC=&
-        # PRG -> JAP not on Azair
-    ]
-    '''
-    rules = (
-        Rule(LinkExtractor(allow=(), restrict_css=('.pageNextPrev',)),
-             callback="parse_item",
-             follow=True),)
+    AUTOTHROTTLE_START_DELAY = 3
+    AUTOTHROTTLE_MAX_DELAY = 7
 
-    def parse_item(self, response):
-        print('Processing' + response.url + '...')
-    '''
+    def urls_to_scrape(self):
+        start_urls = []
+
+        target_destinations = [
+            '&dstAirport=Faro+%5BFAO%5D&dstTypedText=faro',    # PRG -> FAO
+            '&dstAirport=Reykjavik+%28Keflavik%29+%5BKEF%5D',    # PRG -> KEF
+            '&dstAirport=Tromso+%5BTOS%5D&dstTypedText=tromso',    # PRG -> TOS
+            '&dstAirport=Dublin+%5BDUB%5D&dstTypedText=dublin'    # PRG -> DUB
+            # PRG -> JAP not on Azair
+
+        ]
+
+        dates_range = [
+            '&dstFreeAirport=&depdate=25.1.2019&arrdate=31.5.2019',
+            '&dstFreeAirport=&depdate=1.6.2019&arrdate=31.10.2019'
+        ]
+
+        for destination in target_destinations:
+            for date_range in dates_range:
+                start_urls.append(
+                    'http://www.azair.cz/azfin.php?tp=0&searchtype=flexi&srcAirport=Praha+%5BPRG%5D'
+                    '&srcTypedText=prg&srcFreeTypedText=&srcMC=&srcFreeAirport='
+                    + str(destination) +
+                    '&dstFreeTypedText=&dstMC=&adults=1&children=0&infants=0'
+                    '&minHourStay=0%3A45&maxHourStay=23%3A20'
+                    '&minHourOutbound=0%3A00&maxHourOutbound=24%3A00'
+                    '&minHourInbound=0%3A00&maxHourInbound=24%3A00'
+                    + str(date_range) +
+                    '&minDaysStay=3&maxDaysStay=9'
+                    '&nextday=0&autoprice=true&currency=CZK'
+                    '&wizzxclub=false&supervolotea=false&schengen=false&transfer=false'
+                    '&samedep=true&samearr=false&dep0=true'
+                    '&dep1=true&dep2=true&dep3=true&dep4=true&dep5=true&dep6=true'
+                    '&arr0=true&arr1=true&arr2=true&arr3=true&arr4=true&arr5=true&arr6=true'
+                    '&maxChng=1&isOneway=oneway&resultSubmit=Hledat#')
+        return start_urls
+
+    start_urls = urls_to_scrape()
+
+
+    present_timestamp = datetime.now().strftime("%d.%B.%Y %I:%M%p").split(' ')[0]
+    present_day = datetime.now().strftime("%A %d.%B.%Y.%I:%M%p").split(' ')[0]
+
+    logging.INFO('Processing' + response.url + '...')
+
+
+    def process_links(self):
 
     def parse(self, response):
         SET_SELECTOR = '.result '
@@ -43,6 +70,7 @@ class FlightsSpider(CrawlSpider):
             ARR_AER_CODE = '/html[1]/body[1]/div[5]/div[1]/div[1]/p[1]/span[4]/span[1]/text()'
             DEPT_DATE = '.date ::text'    # st\xa029.05.2019
             DEPT_TIME = '.from ::text'
+            AIRLINES = '.airline ::text'
             ARR_TIME = '.to ::text'    # 20:55 Faro[mezera]
             DURATION = '.durcha ::text'    # 6:50 h / 1 p5estup
             WHOLE_PRICE = '.subPrice ::text'    # 1201 K4
@@ -62,6 +90,7 @@ class FlightsSpider(CrawlSpider):
                 'time of departure (flight 1)': flight_info_box.css(DEPT_TIME).extract()[0], # 1
                 'arrival airport (flight 1)': flight_info_box.css(DEPT_AER_CODE).extract()[3],
                 'time of arrival (flight 1)': flight_info_box.css(ARR_TIME).extract()[2],
+                'airlines (flight 1)': flight_info_box.css(AIRLINES).extract_first,    # check
                 'flight fare (flight 1)': flight_info_box.css(PRICE_FOR_SEPARATE_FLIGHT).extract()[0],
                 'seats for lower price (flight 1)': flight_info_box.css(SEATS_FOR_GIVEN_PRICE).extract_first(),
 
@@ -69,6 +98,7 @@ class FlightsSpider(CrawlSpider):
                 'time of departure (flight 2)': flight_info_box.css(DEPT_TIME).extract()[3],  # 1 PRG i u 2 PRG
                 'arrival airport (flight 2)': flight_info_box.xpath(ARR_AER_CODE).extract_first(),
                 'time of arrival (flight 2)': flight_info_box.css(ARR_TIME).extract_first(),
+                'airlines (flight 2)': flight_info_box.css(AIRLINES).extract()[1],    # check
                 'flight fare (flight 2)': flight_info_box.css(PRICE_FOR_SEPARATE_FLIGHT).extract()[1],
                 'seats for lower price (flight 2)': flight_info_box.css(SEATS_FOR_GIVEN_PRICE).extract()[1],
             }
@@ -80,3 +110,31 @@ class FlightsSpider(CrawlSpider):
             #    'created_at': item[2],
             #    'comments': item[3],
             #}
+
+    def create_database(self):
+        conn = sqlite3.connect('flights.db')
+        cur = conn.cursor()
+        cur.execute('CREATE TABLE Flights('
+                    'Date of download NUMERIC, '
+                    'Date of departure NUMERIC, '
+                    'Departure airport TEXT, '
+                    'Departure time NUMERIC, '
+                    'Arrival airport TEXT, '
+                    'Arrival time NUMERIC, '
+                    'Airlines TEXT'
+                    'Fare TEXT'
+                    'Currency TEXT'
+                    'Seats for lower price TEXT')
+        # cur.execute('INSERT INTO Flights (Date of departure,Departure airport,...) VALUES (a, 'c', ...)
+        cur.execute('INSERT INTO Flights VALUES('24.1.2019', 'druhe')
+
+        cur.execute('SELECT * FROM Flights')
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
+
+        cur.close()
+        conn.commit()
+        conn.close()
