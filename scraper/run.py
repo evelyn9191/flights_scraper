2ac547723ccc59d54e6e6f31df7ed250bb413ce5
Michaela Ockova <ockova.michaela@gmail.com>
2020-02-28 22:34:19 +0100

    Move scraper related Python files to a separate module

--- scraper/run.py
+++ scraper/run.py
@@ -3,9 +3,9 @@
 import os
 from datetime import datetime
 
-from scraper import run_spider, edit_data
-from database import create_database, import_to_database
-from charts import create_charts
+from scraper.scraper import run_spider, edit_data
+from scraper.database import create_database, import_to_database
+from scraper.charts import create_charts
 
 output_csv = run_spider()
 formatted_output = edit_data(path_to_data=output_csv)
@@ -13,7 +13,7 @@ formatted_output = edit_data(path_to_data=output_csv)
 if os.path.exists('flights.db') is False:
     create_database()
 import_to_database(path_to_data=formatted_output)
-os.rename(formatted_output, f'imported_files/{formatted_output}')
+os.rename(formatted_output, f'../imported_files//{formatted_output}')
 
 dates_to_run_spider = ['01.03.2019', '15.03.2019', '01.04.2019', '15.04.2019', '01.05.2019',
                        '15.05.2019', '01.06.2019', '15.06.2019', '01.07.2019', '20.07.2019']
